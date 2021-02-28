6c7c395e616eb92901e13a05518e0632cbf02a0c
Vitor Barbosa <vitorbarbosa1998@hotmail.com>
2021-02-28 17:39:29 +0000

    feat: completed component challengebox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { CompletedChallenge } from "../components/CompletedChallenge";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import Head from "next/head";
 import styles from "../styles/pages/Home.module.css";
@@ -18,7 +19,9 @@ export default function Home() {
           <CompletedChallenge />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
